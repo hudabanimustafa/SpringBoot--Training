5f7c79a09da94fb45e4e9bf7b26379bade8c70c2
hudabanimustafa <71215108+hudabanimustafa@users.noreply.github.com>
2023-02-28 23:04:17 +0300

    Add files via upload

---
+++ ProductDao.java
@@ -0,0 +1,47 @@
+package com.hudabanimustafa.pos.pos.dao;
+
+import com.hudabanimustafa.pos.pos.dto.OrderDto;
+import com.hudabanimustafa.pos.pos.entity.OrderEntity;
+import com.hudabanimustafa.pos.pos.entity.ProductEntity;
+import com.hudabanimustafa.pos.pos.entity.SaleEntity;
+import com.hudabanimustafa.pos.pos.repository.ProductRepo;
+import com.hudabanimustafa.pos.pos.repository.SaleRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ProductDao {
+    @Autowired
+    private ProductRepo productRepo;
+
+
+    public ProductEntity addProduct(ProductEntity product) {
+        return this.productRepo.save(product);
+    }
+
+    public ProductEntity updateProduct(ProductEntity product) {
+        return this.productRepo.save(product);
+    }
+
+    public void deleteProduct(Integer productId) {
+        this.productRepo.deleteById(productId);
+    }
+
+    public ProductEntity getProductByBarcode(String barcode) {
+        Optional<ProductEntity> o = Optional.ofNullable(this.productRepo.findByBarcode(barcode));
+        return o.isPresent() ? o.get() : null;
+    }
+    public ProductEntity getProductById(Integer productId) {
+        Optional<ProductEntity> o = this.productRepo.findById(productId);
+        return o.isPresent() ? o.get() : null;
+    }
+    public List<ProductEntity> getAllProducts() {
+        return this.productRepo.findAll();
+    }
+}
